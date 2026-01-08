1232ac64c2db6e98d76648f01eb22d0f20b8d3ee
F4k3r22 <fredyriveraacevedo13@gmail.com>
2026-01-08 17:37:00 -0600

    Update Compiler

--- src/modules/compiler.ts
+++ src/modules/compiler.ts
@@ -1,10 +1,17 @@
 import { build } from 'esbuild';
+import { mkdir } from 'fs/promises';
+import { join } from 'path';
+
 
 export async function ToolCompiler(toolPath: string) {
     try {
+
+        const distPath = join(toolPath, 'dist');
+        await mkdir(distPath, { recursive: true });
+
         await build({
             entryPoints: [toolPath + '/index.ts'],
-            outfile: toolPath + '/index.js',
+            outfile: join(toolPath, 'dist', 'index.js'),
             platform: 'node',
             format: 'esm',
             target: 'node18',
